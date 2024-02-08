d6dbbc035180a8617348b0a80172f63a416ada2b
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-02-08 11:01:13 +0900

    add more operators (exp, sigmoid, different overloads of +-*/ for Value)

--- src/engine.rs
+++ src/engine.rs
@@ -1,3 +1,5 @@
+// base operator (defined as itself): add, mul, neg, pow, exp, relu
+
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::collections::HashSet;
 use std::rc::Rc;
@@ -67,13 +69,20 @@ impl Value {
         out
     }
 
+    pub fn exp(&self) -> Value {
+        let out = Value::new(self.data().exp());
+        out.0.borrow_mut().children.push((self.clone(), self.data().exp()));
+        out
+    }
+    
     pub fn relu(&self) -> Value {
         let out = Value::new(self.data().max(0.0));
-        if self.data() > 0.0 {
-            out.0.borrow_mut().children.push((self.clone(), 1.0))
-        } else {
-            out.0.borrow_mut().children.push((self.clone(), 0.0))
-        }
+        out.0.borrow_mut().children.push((self.clone(), if self.data() > 0.0 { 1.0 } else { 0.0 }));
+        out
+    }
+
+    pub fn sigmoid(&self) -> Value {
+        let out = 1.0 / (1.0 + -self.exp());
         out
     }
 }
@@ -86,41 +95,68 @@ impl Sum for Value {
     }
 }
 
-// operator overloading
 use std::ops::{Add, Sub, Mul, Div, Neg};
 
 impl Add<Value> for Value {
     type Output = Value;
-    fn add(self, other: Self) -> Self::Output {
+    fn add(self: Value, other: Value) -> Self::Output {
         let out = Value::new(self.data() + other.data());
         out.0.borrow_mut().children.push((self.clone(), 1.0));
         out.0.borrow_mut().children.push((other.clone(), 1.0));
         out
     }
 }
+impl Add<f64> for Value {
+    type Output = Value;
+    fn add(self: Value, other: f64) -> Self::Output { self + Value::new(other) }
+}
+impl Add<Value> for f64 {
+    type Output = Value;
+    fn add(self: f64, other: Value) -> Self::Output { Value::new(self) + other }
+}
 
 impl Sub<Value> for Value {
     type Output = Value;
-    fn sub(self, other: Self) -> Self::Output {
-        self + -other
-    }
+    fn sub(self: Value, other: Value) -> Self::Output { self + -other }
+}
+impl Sub<f64> for Value {
+    type Output = Value;
+    fn sub(self: Value, other: f64) -> Self::Output { self + -Value::new(other) }
+}
+impl Sub<Value> for f64 {
+    type Output = Value;
+    fn sub(self: f64, other: Value) -> Self::Output { Value::new(self) + -other }
 }
 
 impl Mul<Value> for Value {
     type Output = Value;
-    fn mul(self, other: Self) -> Self::Output {
+    fn mul(self: Value, other: Value) -> Self::Output {
         let out = Value::new(self.data() * other.data());
         out.0.borrow_mut().children.push((self.clone(), other.data()));
         out.0.borrow_mut().children.push((other.clone(), self.data()));
         out
     }
 }
+impl Mul<f64> for Value {
+    type Output = Value;
+    fn mul(self: Value, other: f64) -> Self::Output { self * Value::new(other) }
+}
+impl Mul<Value> for f64 {
+    type Output = Value;
+    fn mul(self: f64, other: Value) -> Self::Output { Value::new(self) * other }
+}
 
 impl Div<Value> for Value {
     type Output = Value;
-    fn div(self, other: Self) -> Self::Output {
-        self * other.pow(-1.0)
-    }
+    fn div(self: Value, other: Value) -> Self::Output { self * other.pow(-1.0) }
+}
+impl Div<f64> for Value {
+    type Output = Value;
+    fn div(self: Value, other: f64) -> Self::Output { self * Value::new(other).pow(-1.0) }
+}
+impl Div<Value> for f64 {
+    type Output = Value; 
+    fn div(self: f64, other: Value) -> Self::Output { Value::new(self) * other.pow(-1.0) }
 }
 
 impl Neg for Value {
