292fcbdf08cdf1e1091c7556213c00e8b29ed09f
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-02-12 22:07:32 +0900

    reconfigure tensor module

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -1,33 +1,43 @@
 use crate::value::Value;
 
+// support 2D tensors only
 #[derive(Clone)]
 pub struct Tensor {
-    pub data: Vec<Value>,
+    pub data: Vec<Vec<Value>>,
     pub shape: Vec<usize>,
 }
 
 impl Tensor {
-    // support 2D tensors only
-    // TODO: implement 1D, 3D, ... tensors
-    pub fn new(data: Vec<f64>, shape: Vec<usize>) -> Self {
-        Tensor { 
-            data: data.iter().map(|x| Value::new(*x)).collect(), 
-            shape
+    pub fn new(data: Vec<Vec<f64>>, shape: Vec<usize>) -> Self {
+        assert_eq!(shape.len(), 2);
+        assert_eq!(data.len(), shape[0]);
+        assert_eq!(data[0].len(), shape[1]);
+        let mut out = Tensor {
+            data: Vec::new(),
+            shape: shape.clone(),
+        };
+        for i in 0..shape[0] {
+            let mut tmp = Vec::new();
+            for j in 0..shape[1] {
+                tmp.push(Value::new(data[i][j]));
+            }
+            out.data.push(tmp);
         }
+        out
     }
 
-    pub fn data(&self) -> Vec<f64> { self.data.iter().map(|x| x.clone().data()).collect() }
-    pub fn grad(&self) -> Vec<f64> { self.data.iter().map(|x| x.clone().grad()).collect() }
+    pub fn data(&self) -> Vec<Vec<f64>> { self.data.iter().map(|x| x.iter().map(|y| y.data()).collect()).collect() }
+    pub fn grad(&self) -> Vec<Vec<f64>> { self.data.iter().map(|x| x.iter().map(|y| y.grad()).collect()).collect() }
 
     pub fn backward(&self) {
-        for x in self.data.iter() {
-            x.clone().backward();
+        for i in self.data.iter() {
+            for j in i.iter() { j.backward(); }
         }
     }
 
     pub fn log(&self) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().log()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.log()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
@@ -35,7 +45,7 @@ impl Tensor {
 
     pub fn pow(&self, other: f64) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().pow(other)).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.pow(other)).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
@@ -43,7 +53,7 @@ impl Tensor {
 
     pub fn exp(&self) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().exp()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.exp()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
@@ -51,7 +61,7 @@ impl Tensor {
 
     pub fn relu(&self) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().relu()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.relu()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
@@ -59,7 +69,7 @@ impl Tensor {
 
     pub fn sigmoid(&self) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().sigmoid()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.sigmoid()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
@@ -67,110 +77,155 @@ impl Tensor {
 
     pub fn tanh(&self) -> Tensor {
         let out = Tensor {
-            data: self.data.iter().map(|x| x.clone().tanh()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| y.tanh()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
     }
 
-    // support 2D matrix multiplication only
-    pub fn dot(&self, other: Tensor) -> Tensor {
-        let mut out = Tensor::new(vec![0.0; self.shape[0] * other.shape[1]], vec![self.shape[0], other.shape[1]]);
+    pub fn dot(&self, other: &Tensor) -> Tensor {
+        println!("({}, {})", other.shape[0], other.shape[1]);
+        println!("({}, {})", self.shape[0], self.shape[1]);
+
+        assert_eq!(self.shape[1], other.shape[0]);
+        let mut out = Tensor::new(vec![vec![0.0; other.shape[1]]; self.shape[0]], vec![self.shape[0], other.shape[1]]);
         for i in 0..self.shape[0] {
             for j in 0..other.shape[1] {
-                out.data[i * out.shape[1] + j] = (0..self.shape[1]).map(|k| 
-                    self.data[i * self.shape[1] + k].clone() 
-                    * other.data[k * other.shape[1] + j].clone()
+                out.data[i][j] = (0..self.shape[1]).map(
+                    |k| self.data[i][k].clone() * other.data[k][j].clone()
                 ).sum();
+            }
+        }
+        return out
+    }
+
+    pub fn transpose(&self) -> Tensor {
+        assert_eq!(self.shape.len(), 2);
+        let mut out = Tensor::new(vec![vec![0.0; self.shape[0]]; self.shape[1]], vec![self.shape[1], self.shape[0]]);
+        for i in 0..self.shape[0] {
+            for j in 0..self.shape[1] {
+                out.data[j][i] = self.data[i][j].clone();
+            }
+        }
+        out
+    }
+
+    pub fn softmax(&self) -> Tensor {
+        let mut out = Tensor::new(vec![vec![0.0; self.shape[1]]; self.shape[0]], self.shape.clone());
+        for c in 0..self.shape[1] {
+            let mut tmp = Vec::new();
+            for r in 0..self.shape[0] {
+                tmp.push(self.data[r][c].clone());
+            }
+            let sum: Value = tmp.iter().map(|x| x.clone().exp()).sum();
+            for r in 0..self.shape[0] {
+                out.data[r][c] = tmp[r].clone() / sum.clone();
             }
         }
         out
     }
 }
 
-use std::ops::{Add, Sub, Mul, Div, Neg};
+use std::cmp::max;
+use std::ops::{Add, Div, Mul, Neg, Sub};
 impl Add<Tensor> for Tensor {
     type Output = Tensor;
     fn add(self: Tensor, other: Tensor) -> Self::Output {
-        let out = Tensor {
-            data: self.data.iter().zip(other.data.iter()).map(|(x, y)| x.clone() + y.clone()).collect(),
-            shape: self.shape.clone(),
-        };
+        let row_len = max(self.shape[0], other.shape[0]);
+        let col_len = max(self.shape[1], other.shape[1]);
+        let mut out = Tensor::new(vec![vec![0.0; col_len]; row_len], vec![row_len, col_len]);
+        for i in 0..row_len {
+            for j in 0..col_len {
+                out.data[i][j] = self.data[i % self.shape[0]][j % self.shape[1]].clone() + other.data[i % other.shape[0]][j % other.shape[1]].clone();
+            }
+        }
         out
     }
 }
 impl Add<f64> for Tensor {
     type Output = Tensor;
-    fn add(self: Tensor, other: f64) -> Self::Output { self + Tensor::new(vec![other], vec![1, 1]) }
+    fn add(self: Tensor, other: f64) -> Self::Output { self + Tensor::new(vec![vec![other]], vec![1, 1]) }
 }
 impl Add<Tensor> for f64 {
     type Output = Tensor;
-    fn add(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) + other }
+    fn add(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![vec![self]], vec![1, 1]) + other }
 }
 
 impl Sub<Tensor> for Tensor {
     type Output = Tensor;
     fn sub(self: Tensor, other: Tensor) -> Self::Output {
-        let out = Tensor {
-            data: self.data.iter().zip(other.data.iter()).map(|(x, y)| x.clone() - y.clone()).collect(),
-            shape: self.shape.clone(),
-        };
+        let row_len = max(self.shape[0], other.shape[0]);
+        let col_len = max(self.shape[1], other.shape[1]);
+        let mut out = Tensor::new(vec![vec![0.0; col_len]; row_len], vec![row_len, col_len]);
+        for i in 0..row_len {
+            for j in 0..col_len {
+                out.data[i][j] = self.data[i % self.shape[0]][j % self.shape[1]].clone() - other.data[i % other.shape[0]][j % other.shape[1]].clone();
+            }
+        }
         out
     }
 }
 impl Sub<f64> for Tensor {
     type Output = Tensor;
-    fn sub(self: Tensor, other: f64) -> Self::Output { self + -Tensor::new(vec![other], vec![1, 1]) }
+    fn sub(self: Tensor, other: f64) -> Self::Output { self + -Tensor::new(vec![vec![other]], vec![1, 1]) }
 }
 impl Sub<Tensor> for f64 {
     type Output = Tensor;
-    fn sub(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) + -other }
+    fn sub(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![vec![self]], vec![1, 1]) + -other }
 }
 
 // element-wise multiplication
 impl Mul<Tensor> for Tensor { 
     type Output = Tensor;
     fn mul(self: Tensor, other: Tensor) -> Self::Output {
-        let out = Tensor {
-            data: self.data.iter().zip(other.data.iter()).map(|(x, y)| x.clone() * y.clone()).collect(),
-            shape: self.shape.clone(),
-        };
+        let row_len = max(self.shape[0], other.shape[0]);
+        let col_len = max(self.shape[1], other.shape[1]);
+        let mut out = Tensor::new(vec![vec![0.0; col_len]; row_len], vec![row_len, col_len]);
+        for i in 0..row_len {
+            for j in 0..col_len {
+                out.data[i][j] = self.data[i % self.shape[0]][j % self.shape[1]].clone() * other.data[i % other.shape[0]][j % other.shape[1]].clone();
+            }
+        }
         out
     }
 }
 impl Mul<f64> for Tensor {
     type Output = Tensor;
-    fn mul(self: Tensor, other: f64) -> Self::Output { self * Tensor::new(vec![other], vec![1, 1]) }
+    fn mul(self: Tensor, other: f64) -> Self::Output { self * Tensor::new(vec![vec![other]], vec![1, 1]) }
 }
 impl Mul<Tensor> for f64 {
     type Output = Tensor;
-    fn mul(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) * other }
+    fn mul(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![vec![self]], vec![1, 1]) * other }
 }
 
 impl Div<Tensor> for Tensor {
     type Output = Tensor;
     fn div(self: Tensor, other: Tensor) -> Self::Output {
-        let out = Tensor {
-            data: self.data.iter().zip(other.data.iter()).map(|(x, y)| x.clone() / y.clone()).collect(),
-            shape: self.shape.clone(),
-        };
+        let row_len = max(self.shape[0], other.shape[0]);
+        let col_len = max(self.shape[1], other.shape[1]);
+        let mut out = Tensor::new(vec![vec![0.0; col_len]; row_len], vec![row_len, col_len]);
+        for i in 0..row_len {
+            for j in 0..col_len {
+                out.data[i][j] = self.data[i % self.shape[0]][j % self.shape[1]].clone() / other.data[i % other.shape[0]][j % other.shape[1]].clone();
+            }
+        }
         out
     }
 }
 impl Div<f64> for Tensor {
     type Output = Tensor;
-    fn div(self: Tensor, other: f64) -> Self::Output { self * Tensor::new(vec![other], vec![1, 1]).pow(-1.0) }
+    fn div(self: Tensor, other: f64) -> Self::Output { self * Tensor::new(vec![vec![other]], vec![1, 1]).pow(-1.0) }
 }
 impl Div<Tensor> for f64 {
     type Output = Tensor; 
-    fn div(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) * other.pow(-1.0) }
+    fn div(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![vec![self]], vec![1, 1]) * other.pow(-1.0) }
 }
 
 impl Neg for Tensor {
     type Output = Tensor;
     fn neg(self) -> Self::Output {
         let out = Tensor {
-            data: self.data.iter().map(|x| -x.clone()).collect(),
+            data: self.data.iter().map(|x| x.iter().map(|y| -y.clone()).collect()).collect(),
             shape: self.shape.clone(),
         };
         out
--- tests/test_tensor.rs
+++ tests/test_tensor.rs
@@ -3,28 +3,40 @@ use rustgrad::Tensor;
 
 #[test]
 fn test_tensor() {
-    let x = Tensor::new(vec![-4.0]);
+    let x = Tensor::new(vec![vec![-4.0]], vec![1, 1]);
     let z = 2.0 * x.clone() + 2.0 + x.clone();
     let q = z.relu() + z.clone() * x.clone();
     let h = (z.clone() * z.clone()).relu();
     let y = h.clone() + q.clone() + q.clone() * x.clone();
     y.backward();
 
-    assert_eq!(x.data(), vec![-4.0]);
-    assert_eq!(x.grad(), vec![46.0]);
-    assert_eq!(y.data(), vec![-20.0]);
-    assert_eq!(y.grad(), vec![1.0]);
+    assert_eq!(x.data(), vec![vec![-4.0]]);
+    assert_eq!(x.grad(), vec![vec![46.0]]);
+    assert_eq!(y.data(), vec![vec![-20.0]]);
+    assert_eq!(y.grad(), vec![vec![1.0]]);
 }
 
 #[test]
 fn test_tensor_matmul() {
-    let x = Tensor::new(vec![1.0, 2.0, 3.0, 4.0]);
-    let y = Tensor::new(vec![5.0, 6.0, 7.0, 8.0]);
-    let z = x.dot(y.clone());
+    std::env::set_var("RUST_BACKTRACE", "1");
+
+    let x = Tensor::new(vec![vec![1.0, 2.0, 3.0, 4.0]], vec![1, 4]);
+    let y = Tensor::new(vec![vec![5.0], vec![6.0], vec![7.0], vec![8.0]], vec![4, 1]);
+    let z = x.dot(&y);
     z.backward();
 
-    assert_eq!(z.data(), vec![70.0]);
-    assert_eq!(z.grad(), vec![1.0]);
-    assert_eq!(x.grad(), vec![5.0, 6.0, 7.0, 8.0]);
-    assert_eq!(y.grad(), vec![1.0, 2.0, 3.0, 4.0]);
+    assert_eq!(z.data(), vec![vec![70.0]]);
+    assert_eq!(z.grad(), vec![vec![1.0]]);
+    assert_eq!(x.grad(), vec![vec![5.0, 6.0, 7.0, 8.0]]);
+    assert_eq!(y.grad(), vec![vec![1.0], vec![2.0], vec![3.0], vec![4.0]]);
+
+    let p = Tensor::new(vec![vec![1.0], vec![2.0], vec![3.0], vec![4.0]], vec![4, 1]);
+    let q = Tensor::new(vec![vec![5.0], vec![6.0], vec![7.0], vec![8.0]], vec![4, 1]);
+    let r = p.transpose().dot(&q);
+    r.backward();
+
+    assert_eq!(r.data(), vec![vec![70.0]]);
+    assert_eq!(r.grad(), vec![vec![1.0]]);
+    assert_eq!(p.grad(), vec![vec![5.0], vec![6.0], vec![7.0], vec![8.0]]);
+    assert_eq!(q.grad(), vec![vec![1.0], vec![2.0], vec![3.0], vec![4.0]]);
 }
