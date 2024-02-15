acb87611410085e4297e223601b08c95ee59af5a
Jeong Woo-kyung <77jwk0724@gmail.com>
2024-02-15 16:12:35 +0900

    clean up tensor module

--- src/tensor/mod.rs
+++ src/tensor/mod.rs
@@ -13,174 +13,7 @@ pub struct Tensor {
     pub shape: Vec<usize>,
 }
 
-impl Tensor {
-    pub fn hash(&self, coor: &[usize]) -> usize {
-        // assert!(coor.len() == self.shape.len());
-        let mut out: usize = 0;
-        for (i, _) in coor.iter().enumerate() {
-            // if coor[i] >= self.shape[i] {
-            //     println!("coor: {:?}", coor);
-            //     println!("shape: {:?}", self.shape);
-            //     panic!("coordinate out of range");
-            // }
-            out = out * self.shape[i] + coor[i];
-        }
-        // if out >= self.data.len() {
-        //     println!("coor: {:?}", coor);
-        //     println!("shape: {:?}", self.shape);
-        //     println!("out: {}", out);
-        //     println!("data.len(): {}", self.data.len());
-        //     panic!("coordinate out of range");
-        // }
-        out
-    }
-    pub fn get(&self, x: usize, y: usize) -> Value { self.data[self.hash(&[x, y])].clone() }
-    pub fn set(&mut self, x: usize, y: usize, value: Value) {
-        let hash_num = self.hash(&[x, y]);
-        self.data[hash_num] = value;
-    }
-
-    pub fn new(data: Vec<f64>, shape: Vec<usize>) -> Self {        
-        let out = Arc::new(RwLock::new(Tensor {
-            data: Vec::new(),
-            shape: shape.clone()
-        }));
-        out.write().unwrap().data.resize(data.len(), Value::new(0.0));
-        if shape.len() == 2 {
-            (0..shape[0]).into_par_iter().for_each(|i| {
-                (0..shape[1]).for_each(|j| {
-                    let hash_num = out.read().unwrap().hash(&[i, j]);
-                    out.write().unwrap().data[hash_num] = Value::new(data[hash_num]);
-                })
-            });
-        } else { panic!("shape length not supported"); }
-
-        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
-    }
-
-    pub fn data(&self) -> Vec<f64> { self.data.par_iter().map(|x| x.0.read().unwrap().data).collect() }
-    pub fn grad(&self) -> Vec<f64> { self.data.par_iter().map(|x| x.0.read().unwrap().grad).collect() }
-
-    pub fn backward(&self) {
-        self.data.iter().for_each(|i| { i.backward(); });
-    }
-
-    pub fn log(&self) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.log()).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    pub fn pow(&self, other: f64) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.pow(other)).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    pub fn exp(&self) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.exp()).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    pub fn relu(&self) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.relu()).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    pub fn sigmoid(&self) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.sigmoid()).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    pub fn tanh(&self) -> Tensor {
-        Tensor {
-            data: self.data.par_iter().map(|x| x.tanh()).collect(),
-            shape: self.shape.clone(),
-        }
-    }
-
-    // TODO: support 1D, 3D, 4D ... tensors
-    pub fn dot(&self, other: &Tensor) -> Tensor {
-        // println!("({}, {})", other.shape[0], other.shape[1]);
-        // println!("({}, {})", self.shape[0], self.shape[1]);
-
-        // assert_eq!(self.shape.len(), 2);
-        // assert_eq!(other.shape.len(), 2);
-        // assert_eq!(self.shape[1], other.shape[0]);
-        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.shape[0] * other.shape[1]], vec![self.shape[0], other.shape[1]])));
-        (0..self.shape[0]).into_par_iter().for_each(|i| {
-            (0..other.shape[1]).for_each(|j| {
-                let sum = (0..self.shape[1]).into_par_iter().map(|k|
-                    self.get(i, k) * other.get(k, j)
-                ).sum::<Value>();
-                let hash_num = out.read().unwrap().hash(&[i, j]);
-                out.write().unwrap().data[hash_num] = sum;
-            });
-        });
-        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
-    }
-
-    // TODO: support 1D, 3D, 4D ... tensors
-    pub fn transpose(&self) -> Tensor {
-        assert_eq!(self.shape.len(), 2);
-        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.data.len()], vec![self.shape[1], self.shape[0]])));
-        (0..self.shape[0]).into_par_iter().for_each(|i| {
-            (0..self.shape[1]).for_each(|j| {
-                let hash_num = out.read().unwrap().hash(&[j, i]);
-                out.write().unwrap().data[hash_num] = self.get(i, j);
-            });
-        });
-        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
-    }
-
-    // TODO: support 1D, 3D, 4D ... tensors
-    pub fn softmax(&self) -> Tensor {
-        assert_eq!(self.shape.len(), 2);
-        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.data.len()], self.shape.clone())));
-        (0..self.shape[1]).into_par_iter().for_each(|c| {
-            let mut tmp: Vec<Value> = Vec::new();
-            for r in 0..self.shape[0] {
-                tmp.push(self.get(r, c).exp());
-            }
-            let sum: Value = tmp.par_iter().map(|x| x.clone()).sum();
-            for (r, _) in tmp.iter().enumerate().take(self.shape[0]) {
-                let hash_num = out.read().unwrap().hash(&[r, c]);
-                out.write().unwrap().data[hash_num] = tmp[r].clone() / sum.clone();
-            }
-            // let test_sum: f64 = tmp.par_iter().map(|x| x.clone()).sum::<Value>().data() / sum.data();
-            // if !(0.999 < test_sum && test_sum < 1.001) {
-            //     println!("out: {:?}", tmp.iter().map(|x| x.data()).collect::<Vec<f64>>());
-            //     println!("sum: {}", test_sum);
-            //     panic!("sum is not 1.0");
-            // }
-        });
-        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
-    }
-    
-    // TODO: support 1D, 3D, 4D ... tensors
-    pub fn slice(&self, s: std::ops::Range<usize>) -> Tensor {
-        assert_eq!(self.shape.len(), 2);
-        assert!(s.start < s.end);
-        assert!(s.end <= self.shape[1]);
-        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; (s.end - s.start) * self.shape[0]], vec![self.shape[0], s.end - s.start])));
-        (0..self.shape[0]).into_par_iter().for_each(|i| {
-            (s.start..s.end).for_each(|j| {
-                out.write().unwrap().set(i, j - s.start, self.get(i, j));
-            })
-        });
-        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
-    }
-}
-
-// TODO: fix broadcasting bug
+// base ops: add, mul, neg
 impl Add<Tensor> for Tensor {
     type Output = Tensor;
     fn add(self: Tensor, other: Tensor) -> Self::Output {
@@ -202,20 +35,6 @@ impl Add<Tensor> for f64 {
     type Output = Tensor;
     fn add(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) + other }
 }
-
-impl Sub<Tensor> for Tensor {
-    type Output = Tensor;
-    fn sub(self: Tensor, other: Tensor) -> Self::Output { self + -other }
-}
-impl Sub<f64> for Tensor {
-    type Output = Tensor;
-    fn sub(self: Tensor, other: f64) -> Self::Output { self + -Tensor::new(vec![other], vec![1, 1]) }
-}
-impl Sub<Tensor> for f64 {
-    type Output = Tensor;
-    fn sub(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) + -other }
-}
-
 impl Mul<Tensor> for Tensor { 
     type Output = Tensor;
     fn mul(self: Tensor, other: Tensor) -> Self::Output {
@@ -237,7 +56,29 @@ impl Mul<Tensor> for f64 {
     type Output = Tensor;
     fn mul(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) * other }
 }
+impl Neg for Tensor {
+    type Output = Tensor;
+    fn neg(self) -> Self::Output {
+        Tensor {
+            data: self.data.par_iter().map(|x| -x.clone()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+}
 
+// more ops: sub, div
+impl Sub<Tensor> for Tensor {
+    type Output = Tensor;
+    fn sub(self: Tensor, other: Tensor) -> Self::Output { self + -other }
+}
+impl Sub<f64> for Tensor {
+    type Output = Tensor;
+    fn sub(self: Tensor, other: f64) -> Self::Output { self + -Tensor::new(vec![other], vec![1, 1]) }
+}
+impl Sub<Tensor> for f64 {
+    type Output = Tensor;
+    fn sub(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) + -other }
+}
 impl Div<Tensor> for Tensor {
     type Output = Tensor;
     fn div(self: Tensor, other: Tensor) -> Self::Output { self * other.pow(-1.0) }
@@ -251,12 +92,135 @@ impl Div<Tensor> for f64 {
     fn div(self: f64, other: Tensor) -> Self::Output { Tensor::new(vec![self], vec![1, 1]) * other.pow(-1.0) }
 }
 
-impl Neg for Tensor {
-    type Output = Tensor;
-    fn neg(self) -> Self::Output {
+impl Tensor {
+    pub fn new(data: Vec<f64>, shape: Vec<usize>) -> Self {        
+        let out = Arc::new(RwLock::new(Tensor {
+            data: Vec::new(),
+            shape: shape.clone()
+        }));
+        out.write().unwrap().data.resize(data.len(), Value::new(0.0));
+        if shape.len() == 2 {
+            (0..shape[0]).into_par_iter().for_each(|i| {
+                (0..shape[1]).for_each(|j| {
+                    let hash_num = out.read().unwrap().hash(&[i, j]);
+                    out.write().unwrap().data[hash_num] = Value::new(data[hash_num]);
+                })
+            });
+        } else { panic!("shape length not supported"); }
+
+        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
+    }
+
+    // base ops: pow, exp, relu, log
+    pub fn pow(&self, other: f64) -> Tensor {
         Tensor {
-            data: self.data.par_iter().map(|x| -x.clone()).collect(),
+            data: self.data.par_iter().map(|x| x.pow(other)).collect(),
             shape: self.shape.clone(),
         }
     }
+    pub fn exp(&self) -> Tensor {
+        Tensor {
+            data: self.data.par_iter().map(|x| x.exp()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+    pub fn relu(&self) -> Tensor {
+        Tensor {
+            data: self.data.par_iter().map(|x| x.relu()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+    pub fn log(&self) -> Tensor {
+        Tensor {
+            data: self.data.par_iter().map(|x| x.log()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+
+    // more ops: dot, softmax, sigmoid, tanh
+    pub fn dot(&self, other: &Tensor) -> Tensor {
+        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.shape[0] * other.shape[1]], vec![self.shape[0], other.shape[1]])));
+        (0..self.shape[0]).into_par_iter().for_each(|i| {
+            (0..other.shape[1]).for_each(|j| {
+                let sum = (0..self.shape[1]).into_par_iter().map(|k|
+                    self.get(i, k) * other.get(k, j)
+                ).sum::<Value>();
+                let hash_num = out.read().unwrap().hash(&[i, j]);
+                out.write().unwrap().data[hash_num] = sum;
+            });
+        });
+        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
+    }
+    pub fn softmax(&self) -> Tensor {
+        assert_eq!(self.shape.len(), 2);
+        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.data.len()], self.shape.clone())));
+        (0..self.shape[1]).into_par_iter().for_each(|c| {
+            let mut tmp: Vec<Value> = Vec::new();
+            for r in 0..self.shape[0] {
+                tmp.push(self.get(r, c).exp());
+            }
+            let sum: Value = tmp.par_iter().map(|x| x.clone()).sum();
+            for (r, _) in tmp.iter().enumerate().take(self.shape[0]) {
+                let hash_num = out.read().unwrap().hash(&[r, c]);
+                out.write().unwrap().data[hash_num] = tmp[r].clone() / sum.clone();
+            }
+        });
+        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
+    }
+    pub fn sigmoid(&self) -> Tensor {
+        Tensor {
+            data: self.data.par_iter().map(|x| x.sigmoid()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+    pub fn tanh(&self) -> Tensor {
+        Tensor {
+            data: self.data.par_iter().map(|x| x.tanh()).collect(),
+            shape: self.shape.clone(),
+        }
+    }
+
+    // backward prop
+    pub fn backward(&self) {
+        self.data.iter().for_each(|i| { i.backward(); });
+    }
+
+    // misc
+    pub fn transpose(&self) -> Tensor {
+        assert_eq!(self.shape.len(), 2);
+        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; self.data.len()], vec![self.shape[1], self.shape[0]])));
+        (0..self.shape[0]).into_par_iter().for_each(|i| {
+            (0..self.shape[1]).for_each(|j| {
+                let hash_num = out.read().unwrap().hash(&[j, i]);
+                out.write().unwrap().data[hash_num] = self.get(i, j);
+            });
+        });
+        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
+    }
+    pub fn slice(&self, s: std::ops::Range<usize>) -> Tensor {
+        assert_eq!(self.shape.len(), 2);
+        assert!(s.start < s.end);
+        assert!(s.end <= self.shape[1]);
+        let out = Arc::new(RwLock::new(Tensor::new(vec![0.0; (s.end - s.start) * self.shape[0]], vec![self.shape[0], s.end - s.start])));
+        (0..self.shape[0]).into_par_iter().for_each(|i| {
+            (s.start..s.end).for_each(|j| {
+                out.write().unwrap().set(i, j - s.start, self.get(i, j));
+            })
+        });
+        Arc::try_unwrap(out).unwrap().into_inner().unwrap()
+    }
+    pub fn hash(&self, coor: &[usize]) -> usize {
+        let mut out: usize = 0;
+        for (i, _) in coor.iter().enumerate() {
+            out = out * self.shape[i] + coor[i];
+        }
+        out
+    }
+    pub fn get(&self, x: usize, y: usize) -> Value { self.data[self.hash(&[x, y])].clone() }
+    pub fn set(&mut self, x: usize, y: usize, value: Value) {
+        let hash_num = self.hash(&[x, y]);
+        self.data[hash_num] = value;
+    }
+    pub fn data(&self) -> Vec<f64> { self.data.par_iter().map(|x| x.0.read().unwrap().data).collect() }
+    pub fn grad(&self) -> Vec<f64> { self.data.par_iter().map(|x| x.0.read().unwrap().grad).collect() }
 }
